3b11386bc3160df8c7296ac906c8baad5f7a2048
Remi Ferrand <remi.ferrand@cc.in2p3.fr>
2018-06-25 11:36:26 +0200

    Use external module ccin2p3/etc_services

--- manifests/config/etc_services.pp
+++ manifests/config/etc_services.pp
@@ -1,39 +1,15 @@
 #
 class mit_krb5::config::etc_services {
 
-	$port = 88
-	$comment = 'Kerberos v5'
+  [ 'tcp', 'udp' ].each |String $protocol| {
 
-	augeas { 'kerberos_etc_services_tcp':
-		context => '/files/etc/services',
-		changes => [
-			'defnode kerberostcp service-name[.="kerberos"][protocol = "tcp"] kerberos',
-			"set \$kerberostcp/port ${port}",
-			'set $kerberostcp/protocol tcp',
-			'remove $kerberostcp/alias',
-			'remove $kerberostcp/#comment',
-			'set $kerberostcp/alias[1] kerberos5',
-			'set $kerberostcp/alias[2] krb5',
-			'set $kerberostcp/alias[3] kerberos-sec',
-			"set \$kerberostcp/#comment '${comment}'",
-		]
-	}
+    ::etc_services { "kerberos/${protocol}":
+      port    => '88',
+      aliases => [ 'kerberos5', 'krb5', 'kerberos-sec' ],
+      comment => 'Kerberos v5'
+    }
 
-	augeas { 'kerberos_etc_services_udp':
-		context => '/files/etc/services',
-		changes => [
-			'defnode kerberosudp service-name[.="kerberos"][protocol = "udp"] kerberos',
-			"set \$kerberosudp/port ${port}",
-			'set $kerberosudp/protocol udp',
-			'remove $kerberosudp/alias',
-			'remove $kerberosudp/#comment',
-			'set $kerberosudp/alias[1] kerberos5',
-			'set $kerberosudp/alias[2] krb5',
-			'set $kerberosudp/alias[3] kerberos-sec',
-			"set \$kerberosudp/#comment '${comment}'",
-		]
-	}
-
+  }
 }
 
-# vim: tabstop=4 shiftwidth=4 softtabstop=4
+# vim: tabstop=2 shiftwidth=2 softtabstop=2
